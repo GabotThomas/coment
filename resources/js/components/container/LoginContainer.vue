0e1d265986717d33f19fa1b4647c6da0c98f870b
GabotThomas <gabotthomas@gmail.com>
2022-06-15 10:05:32 +0200

    scss form begin

--- resources/js/components/container/LoginContainer.vue
+++ resources/js/components/container/LoginContainer.vue
@@ -42,52 +42,17 @@ watch(resultLogin, (currentValue, oldValue) => {
 </script>
 <template>
   <div class="
-      container
-      mx-auto
-      text-center
-      flex
-      justify-center
-      items-center
-      h-screen
+      ui container login
     ">
     <div class="w-full max-w-xs">
-      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-            Email
-          </label>
-          <input class="
-              shadow
-              appearance-none
-              border
-              rounded
-              w-full
-              py-2
-              px-3
-              text-gray-700
-              leading-tight
-              focus:outline-none focus:shadow-outline
-            " id="username" name="email" type="text" placeholder="Username" @input="handleChange"
+      <form class="ui form">
+        <div class="">
+          <input class="ui input" id="username" name="email" type="text" placeholder="Username" @input="handleChange"
             :value="user.value.email || ''" />
         </div>
         <div class="mb-6">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-            Password
-          </label>
-          <input class="
-              shadow
-              appearance-none
-              border border-red-500
-              rounded
-              w-full
-              py-2
-              px-3
-              text-gray-700
-              mb-3
-              leading-tight
-              focus:outline-none focus:shadow-outline
-            " id="password" type="password" name="password" placeholder="******************" @input="handleChange"
-            :value="user.value.password || ''" />
+          <input class="ui input" id="password" type="password" name="password" placeholder="******************"
+            @input="handleChange" :value="user.value.password || ''" />
           <p class="text-red-500 text-xs italic">Please choose a password.</p>
         </div>
         <div class="flex items-center justify-between">
