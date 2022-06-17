76ecbc020065afe1b27fe9cf3a0f1163429421a7
GabotThomas <gabotthomas@gmail.com>
2022-06-17 09:59:42 +0200

    git add wisiwig

--- resources/js/admin/components/line/QuestionFormLine.vue
+++ resources/js/admin/components/line/QuestionFormLine.vue
@@ -1,12 +1,21 @@
 <script setup>
 import TextArea from '../form/module/Textarea.vue';
 import InputVue from '../form/module/Input.vue';
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
 
 const props = defineProps({
     value: Object,
     handleChange: Function,
     index: Number
 });
+
+const editor = useEditor({
+    content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+    extensions: [
+        StarterKit,
+    ],
+})
 
 const handleChangeController = (e, { name, value }) => {
     props.handleChange(props.index, { [name]: value })
@@ -20,6 +29,7 @@ const handleCheck = (e, { name, checked }) => {
 
 <template>
     <text-area :value="props.value.text" :name="'text'" :handle-change="handleChangeController" :placeholder="'name'" />
+    <editor-content :editor="editor" />
     <InputVue :value="props.value.text" :name="'is_sexist'" :handle-change="handleCheck" :placeholder="'name'"
         :type="'checkbox'" />
 </template>
