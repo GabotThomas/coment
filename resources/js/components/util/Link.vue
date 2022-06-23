e9d204ee9b0c37052925d21daa28946c79e4c7f8
GabotThomas <gabotthomas@gmail.com>
2022-06-23 14:37:38 +0200

    add link

---
+++ resources/js/components/util/Link.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+const props = defineProps({
+    to: Object,
+    disabled: {
+        type: Boolean,
+        default: false
+    }
+});
+
+</script>
+
+<template>
+    <router-link v-if="!disabled" :to="props.to">
+        <slot></slot>
+    </router-link>
+    <div v-else>
+        <slot></slot>
+    </div>
+</template>
