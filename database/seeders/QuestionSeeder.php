9c683e9e75d93c66492fc0ba78a6b8be5e85f139
Fabien <fabienvdbg@gmail.com>
2022-06-24 12:07:01 +0200

    fix question

--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -19,8 +19,9 @@ class QuestionSeeder extends Seeder
         Question::create([
             'id'                    => 1,
             'quiz_id'               => '1',
-            'text'                  => 'Le langage utilisé véhicule-t-il des stéréotypes sexistes ?',
-            'image'                 => '/storage/image/question/raimbow.json',
+            'text'                  => 'Le rôle, l’activité ou le comportement assigné.e.s aux femmes
+            véhicule.nt-il.s/elle.s un ou plusieurs stéréotypes sexistes?',
+            'image'                 => '/storage/image/question/messenger-heart.json',
             'is_sexist'             => true,
             'created_at'            => now(),
             'updated_at'            => now(),
@@ -28,26 +29,106 @@ class QuestionSeeder extends Seeder
         Question::create([
             'id'                    => 2,
             'quiz_id'               => '1',
-            'text'                  => 'Des stéréotypes sexistes ou les violences faites aux femmes sont-ils/
-            elles abordé.e.s sous l’angle de l’humour ou du second degré ?',
+            'text'                  => 'Les métiers assignés aux personnages véhiculent-ils
+            des stéréotypes sexistes ?',
             'image'                 => '/storage/image/question/electric.json',
             'is_sexist'             => true,
             'created_at'            => now(),
             'updated_at'            => now(),
         ]);
-
-        //Level - 1 //Quiz - 2
         Question::create([
             'id'                    => 3,
-            'quiz_id'               => '2',
-            'text'                  => 'Lorem ipsum dolor sit amet ?',
-            'image'                 => '/storage/image/question/raimbow.json',
-            'is_sexist'             => false,
+            'quiz_id'               => '1',
+            'text'                  => 'Un des deux sexes est-il numériquement plus représenté
+            que l’autre et cette surreprésentation illustre-t-elle
+            des stéréotypes sexistes ?',
+            'image'                 => '/storage/image/question/heart.json',
+            'is_sexist'             => true,
             'created_at'            => now(),
             'updated_at'            => now(),
         ]);
         Question::create([
             'id'                    => 4,
+            'quiz_id'               => '1',
+            'text'                  => 'La représentation de la parentalité renvoie-t-elle aux rôles
+            traditionnellement assignés à chaque sexe?',
+            'image'                 => '/storage/image/question/messenger-confetti.json',
+            'is_sexist'             => true,
+            'created_at'            => now(),
+            'updated_at'            => now(),
+        ]);
+        Question::create([
+            'id'                    => 5,
+            'quiz_id'               => '1',
+            'text'                  => 'Les corps sont-ils « objétisés » et mis au service d’une
+            représentation sexiste?',
+            'image'                 => '/storage/image/question/messenger-heart.json',
+            'is_sexist'             => true,
+            'created_at'            => now(),
+            'updated_at'            => now(),
+        ]);
+        Question::create([
+            'id'                    => 6,
+            'quiz_id'               => '1',
+            'text'                  => 'La place et l’attitude renvoient-elles à des stéréotypes sexistes ?',
+            'image'                 => '/storage/image/question/electric.json',
+            'is_sexist'             => true,
+            'created_at'            => now(),
+            'updated_at'            => now(),
+        ]);
+        Question::create([
+            'id'                    => 7,
+            'quiz_id'               => '1',
+            'text'                  => 'Le langage utilisé véhicule-t-il des stéréotypes sexistes?
+            Sont concernés les textes écrits, signatures, dialogues, voix off, chansons,
+            etc',
+            'image'                 => '/storage/image/question/heart.json',
+            'is_sexist'             => true,
+            'created_at'            => now(),
+            'updated_at'            => now(),
+        ]);
+        Question::create([
+            'id'                    => 8,
+            'quiz_id'               => '1',
+            'text'                  => 'Des stéréotypes sexistes ou les violences faites aux femmes sont-ils/
+            elles abordé.e.s sous l’angle de l’humour ou du second degré ?',
+            'image'                 => '/storage/image/question/messenger-confetti.json',
+            'is_sexist'             => true,
+            'created_at'            => now(),
+            'updated_at'            => now(),
+        ]);
+        Question::create([
+            'id'                    => 9,
+            'quiz_id'               => '1',
+            'text'                  => 'Les lieux dans lesquels évolue chacun
+            des personnages véhiculent-ils un ou plusieurs stéréotypes
+            sexistes ?',
+            'image'                 => '/storage/image/question/electric.json',
+            'is_sexist'             => true,
+            'created_at'            => now(),
+            'updated_at'            => now(),
+        ]);
+        Question::create([
+            'id'                    => 10,
+            'quiz_id'               => '1',
+            'text'                  => 'L’image fait-elle référence à des stéréotypes sexistes ?',
+            'image'                 => '/storage/image/question/messenger-heart.json',
+            'is_sexist'             => true,
+            'created_at'            => now(),
+            'updated_at'            => now(),
+        ]);
+
+
+        //Level - 1 //Quiz - 2
+        Question::create([
+            'quiz_id'               => '2',
+            'text'                  => 'Lorem ipsum dolor sit amet ?',
+            'image'                 => '/storage/image/question/heart.json',
+            'is_sexist'             => false,
+            'created_at'            => now(),
+            'updated_at'            => now(),
+        ]);
+        Question::create([
             'quiz_id'               => '2',
             'text'                  => 'Lorem ipsum dolor sit amet. Aut neque exercitationem modi ?',
             'image'                 => '/storage/image/question/electric.json',
@@ -56,7 +137,6 @@ class QuestionSeeder extends Seeder
             'updated_at'            => now(),
         ]);
         Question::create([
-            'id'                    => 5,
             'quiz_id'               => '2',
             'text'                  => 'Lorem ipsum dolor sit amet. Aut neque exercitationem modi ????',
             'image'                 => '/storage/image/question/messenger.json',
